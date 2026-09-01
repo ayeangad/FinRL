9f12da74ce203b3c1b5a20677ef7ed311dff2aa6
Angad <angadsankhla09@gmail.com>
2026-09-02 01:34:04 +0530

    feat: add execution domain model

---
+++ finrl/domain/execution.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from decimal import Decimal
+
+from pydantic import BaseModel, Field
+
+class Execution(BaseModel):
+    order_id: str
+    execution_id: str
+    
+    quantity: Decimal = Field(gt=0)
+    price: Decimal = Field(gt=0)
+    
+    executed_at: datetime
